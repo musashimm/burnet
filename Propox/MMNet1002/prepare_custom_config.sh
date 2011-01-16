317d233e102e9f7c8784e65b45b4aa2a2c63f385
Wojciech Todryk <wojciech@todryk.pl>
2011-01-16 18:53:53 +0100

    name change

--- Propox/MMNet1002/prepare_custom_config.sh
+++ Propox/MMNet1002/prepare_custom_config.sh
@@ -1,5 +1,7 @@
 echo "============= Custom config start ==========================="
 
+echo "============= Copying additional files to root file system =="
+
 CURR_DIR=`pwd`
 cd myboard/target_files
 rsync -av * $1
