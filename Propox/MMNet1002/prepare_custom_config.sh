5aeb5bb79ab6ea959b3b547326e8b25f1a14955c
Wojciech Todryk <wojciech@todryk.pl>
2011-06-17 21:39:44 +0200

    new kernel

--- Propox/MMNet1002/prepare_custom_config.sh
+++ Propox/MMNet1002/prepare_custom_config.sh
@@ -6,10 +6,8 @@ ln -s ../proc/mounts $1/etc/mtab
 ln -s ../tmp/random-seed $1/etc/random-seed
 ln -s ../tmp/resolv.conf $1/etc/resolv.conf
 ln -s ../tmp $1/var/cache
-ln -s ../tmp $1/var/lib/pcmcia
 ln -s ../tmp $1/var/lock
 ln -s ../tmp $1/var/log
-ln -s ../tmp $1/var/pcmcia
 ln -s ../tmp $1/var/run
 ln -s ../tmp $1/var/spool
 ln -s ../tmp $1/var/tmp
